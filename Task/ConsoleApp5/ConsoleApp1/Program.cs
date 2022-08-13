8fd4022058887f7ace2ba968d590a9d364859afd
Dmitriy Solopov <div.solopov@gmail.com>
2022-08-13 15:33:27 +0300

    first commit

---
+++ Task/ConsoleApp5/ConsoleApp1/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static ValueTask CalculateAndShowResult(int data)
+        {
+            if (data > 0)
+            {
+                return new ValueTask(Task.Run(()=> Calculate(data)));
+            }
+            else
+            {
+                return new ValueTask(); //задача закрыта или невалидны аргументы
+            }
+        }
+        static void Calculate (int data)
+        {
+            Console.WriteLine($" Сложная задача: {Task.CurrentId}, началась в потоке: {Thread.CurrentThread.ManagedThreadId}");
+            Console.WriteLine("Сложная задача запущена.");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write(".");
+                Thread.Sleep(100);
+            }
+            Console.WriteLine("Сложная задача завершена.");
+            Random random = new Random();
+            Console.WriteLine($" Результат: {random.Next(0, data)}, Задача № {Task.CurrentId}, началась в потоке: {Thread.CurrentThread.ManagedThreadId}");
+        }
+
+        static ValueTask<int> CalculateSum(int a, int b)
+        {
+            Thread.Sleep(1500);
+            if (a == 0)
+            {
+                return new ValueTask<int>(b);
+            }
+            else if (b == 0)
+            {
+                return new ValueTask<int>(a);
+            } 
+            else if (a == 0 && b == 0)
+            {
+                return new ValueTask<int>(0);
+            }
+            else {
+                return new ValueTask<int>(Task.Run(() => { return a + b; }));
+            }
+        }
+        static void Main(string[] args)
+        {
+            CalculateAndShowResult(-5).GetAwaiter().GetResult();
+            CalculateAndShowResult(15000).GetAwaiter().GetResult();
+
+            Console.WriteLine($"Результат сложения: {CalculateSum(15, 17).Result}");
+
+            ValueTask<int> valTask = CalculateSum(15, 17);
+
+            while (!valTask.IsCompleted)
+            {
+                Console.Write(".");
+                Thread.Sleep(250);
+            }
+            
+            
+            Task<int> task = valTask.AsTask();
+            task.ContinueWith((t) => { Console.WriteLine($"Задача была завершена.... {t.Result}"); });
+            
+        }
+    }
+}
---
+++ Task/ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+
+
+
+    class Program
+    {
+
+        static void ShowInfo()
+        {
+            Console.WriteLine($" Простая задача: {Task.CurrentId}, началась в потоке: {Thread.CurrentThread.ManagedThreadId}");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Важная работа, этап {i}");
+                Thread.Sleep(200);
+            }
+        } 
+
+        static int ShowResult()
+        {
+            Console.WriteLine($" Сложная задача: {Task.CurrentId}, началась в потоке: {Thread.CurrentThread.ManagedThreadId}");
+            Console.WriteLine("Сложная задача запущена.");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write(".");
+                Thread.Sleep(100);
+            }
+            Console.WriteLine("Сложная задача завершена.");
+            Random random = new Random();
+            return random.Next(0, Int32.MaxValue);
+        }
+
+        static int ComplexCalculation(int a, int b)
+        {
+            Console.WriteLine($" Очередная задача: {Task.CurrentId}, выполняется в потоке: {Thread.CurrentThread.ManagedThreadId}");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write(".");
+            }
+            Console.WriteLine();
+            return a + b;
+        }
+        static void ShowComplexCalculationResult(Task<int> task)
+        {
+            Console.WriteLine($"Результатом очередной задачи: {task.Result}");
+        }
+
+        static void OneStage(object argum)
+        {
+            Console.WriteLine($" СверхПакетная задача: {Task.CurrentId}, началась в потоке: {Thread.CurrentThread.ManagedThreadId}");
+            Thread.Sleep(Convert.ToInt32(argum));
+            Console.WriteLine($" СверхПакетная задача: {Task.CurrentId}, завершилась в потоке: {Thread.CurrentThread.ManagedThreadId}");
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Идентификатор основного потока: {Thread.CurrentThread.ManagedThreadId}");
+            Action simpleAction = new Action(ShowInfo);
+            Task simpleTask = new Task(simpleAction);
+
+            simpleTask.Start();
+            simpleTask.Wait(); //команда основному потоку водождать результатов
+
+
+            Func<int> returnedAction = new Func<int>(ShowResult);
+            Task<int> returnedTask = new Task<int>(returnedAction);
+            returnedTask.Start();
+            returnedTask.Wait();
+            Console.WriteLine(returnedTask.Result); //обращение к результату
+
+
+
+            List<Task> tasks = new List<Task>();
+            tasks.Add(new Task(OneStage, 1500));
+            tasks.Add(new Task(OneStage, 1300));
+            tasks.Add(new Task(OneStage, 3500));
+            tasks.Add(new Task(OneStage, 7500));
+            tasks.Add(new Task(OneStage, 6700));
+            tasks.Add(new Task(OneStage, 3000));
+
+            foreach (var item in tasks)
+            {
+                item.Start();
+            }
+            //Task.WaitAny(tasks.ToArray());
+            Task.WaitAll(tasks.ToArray());
+
+            //Console.ReadKey();
+
+
+            Task<int> taskComplexCalculation = new Task<int>(()=> ComplexCalculation(159, 317));
+            Task continuationCallBack = taskComplexCalculation.ContinueWith(new Action<Task<int>>(ShowComplexCalculationResult));
+
+            taskComplexCalculation.Start();
+            continuationCallBack.Wait();
+
+        }
+    }
+}
